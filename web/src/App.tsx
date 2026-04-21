1bbe5b67d6bcd0b429b6e2127526f5b75dcfee02
k4rkie <karkeekrish07@gmail.com>
2026-04-21 19:32:52 +0545

    Add indexer.go

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero"></div>
+        <h1>Get started</h1>
+        <form action="">
+          <input type="text" placeholder="Search..." />
+          <button>🔎</button>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default App;
