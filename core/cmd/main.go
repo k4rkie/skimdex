4a86fed266b92fba747ef168540df0b76e9704d5
k4rkie <karkeekrish07@gmail.com>
2026-04-22 18:42:51 +0545

    Add dirCrawler and improve indexer

--- core/cmd/main.go
+++ core/cmd/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/k4rkie/skimdex-core/internal/crawler"
+	"github.com/k4rkie/skimdex-core/internal/index"
 )
 
 func main() {
-	fmt.Println("Skimdex")
+
+	// NOTE: Just a place holder path
+	rootDir := "path/to/docs"
+
+	filePaths, err := crawler.FindFiles(rootDir)
+	if err != nil {
+		log.Printf("Error finding files in the given directory: %v", err)
+	}
+
+	// Create new instance of the indexer
+	indexer := index.NewIndexer()
+
+	for _, filePath := range filePaths {
+		file, _ := os.Open(filePath)
+		defer file.Close()
+		indexer.RegisterDocStruct(filePath, file)
+	}
+
 }
